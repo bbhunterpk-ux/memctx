128db802d3a94ec0b1d866a651774fd50bfe5883
bbhunterpk-ux <bbhunter.pk@gmail.com>
2026-04-14 21:32:27 +0500

    1

--- MemCTX_Frontend/vite.config.ts
+++ MemCTX_Frontend/vite.config.ts
@@ -21,4 +21,12 @@ export default defineConfig(() => ({
   build: {
     target: "esnext",
   },
+  esbuild: {
+    target: "esnext",
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "esnext",
+    },
+  },
 }));
